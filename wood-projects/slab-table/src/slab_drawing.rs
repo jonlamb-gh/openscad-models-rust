978231c6d4ec225936d5f7b50df42a1572a22270
dev <Jon Lamb>
2018-10-09 12:15:07 -0700

    slab-table: Update slab drawing

--- wood-projects/slab-table/src/slab_drawing.rs
+++ wood-projects/slab-table/src/slab_drawing.rs
@@ -7,15 +7,15 @@ use slab::Slab;
 
 impl DrawingAssembler for Slab {
     fn drawing_params(&self) -> DrawingParams {
-        let delta = 30.0;
+        let delta = 40.0;
         DrawingParams {
             doc_scale: 40.0,
             show_frame: true,
             doc_height: SLAB_WIDTH + 5.0,
             top_left_view_pos: vec3(-SLAB_LENGTH - delta, delta, 0.0),
             top_right_view_pos: vec3(delta * 2.5, delta, 0.0),
-            bottom_left_view_pos: vec3(-SLAB_LENGTH - delta, -POST_WIDTH - delta, 0.0),
-            bottom_right_view_pos: vec3(delta, -POST_THICKNESS - delta, 0.0),
+            bottom_left_view_pos: vec3(-SLAB_LENGTH - delta, -SLAB_THICKNESS - delta, 0.0),
+            bottom_right_view_pos: vec3(delta, -SLAB_THICKNESS - delta, 0.0),
         }
     }
 
